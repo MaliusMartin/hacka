ce196c89de8ea208a0f24cd751456764688c3a6c
MaliusMartin <maliberry7@gmail.com>
2024-03-22 23:02:43 +0300

    fifth

--- womenict/settings.py
+++ womenict/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-77w$_cci#+jbwunduci$t6cngn66(kipb1xa+1e_grkzwq!$ko
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'devlevelprediction.onrender.com']
 
 
 # Application definition
